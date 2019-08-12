c484bb16aa874279653e8959fd3bced2dd10cac3
Fumikazu Fujiwara <github@ze.gs>
2019-08-12 21:27:55 +0900

    ReturnStatement

---
+++ test/return_statement.test.js
@@ -0,0 +1,29 @@
+import ReturnStatement from '../src/return_statement';
+import Token from '../src/token';
+describe('ReturnStatement', () => {
+  describe('constructer', () => {
+    test('new ReturnStatement', () => {
+      let i = new ReturnStatement();
+      expect(i).not.toBeNull();
+      expect(i.node).toBe(undefined);
+      expect(i.token).toBe(undefined);
+      expect(i.name).toBe(undefined);
+      expect(i.value).toBe(undefined);
+    });
+  });
+  describe('statementNode', () => {
+    test('only check the existence', () => {
+      let i = new ReturnStatement();
+      expect(typeof i.statementNode).toBe('function');
+      i.statementNode();
+    });
+  });
+  describe('TokenLiteral', () => {
+    test('only check the existence', () => {
+      let i = new ReturnStatement();
+      expect(typeof i.TokenLiteral).toBe('function');
+      i.token = new Token('INT','10');
+      i.TokenLiteral();
+    });
+  });
+});
