b6dfdd8a6bfbeae9df002d411a68195e4ff5425b
Fumikazu Fujiwara <github@ze.gs>
2019-08-15 07:05:25 +0900

    ExpressionStatement.toString()

--- test/expression_statement.test.js
+++ test/expression_statement.test.js
@@ -1,10 +1,27 @@
-import ExpressionStatement from '../src/return_statement';
 import Token from '../src/token';
+import Identifier from '../src/identifier';
+import LetStatement from '../src/let_statement';
+import ExpressionStatement from '../src/expression_statement';
 describe('ExpressionStatement', () => {
   describe('constructer', () => {
     test('new ExpressionStatement', () => {
       const i = new ExpressionStatement();
       expect(i).not.toBeNull();
+    });
+  });
+  describe('toString', () => {
+    test('string expression', () => {
+      const i = new ExpressionStatement(
+        new Token('INT','10'),
+        new LetStatement(
+          new Token(Token.TOKEN_TYPE.LET,'let'),
+          new Identifier(undefined,'val')
+        )
+      );
+      expect(typeof i.toString).toBe('function');
+      expect(i.Value).toBe(undefined);
+      const str = `${i}`;
+      expect(str).toBe("let val = undefined");
     });
   });
   describe('statementNode', () => {
